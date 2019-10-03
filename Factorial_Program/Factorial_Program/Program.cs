dea3fc50bd08e7b8534d06aedf8d24715a171119
Steven Scott <stevescott2653@gmail.com>
2019-10-03 18:48:22 -0400

    Add files via upload

---
+++ Factorial_Program/Factorial_Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Program
+{
+    public class FactorialExample
+    {
+        public static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.Write("Enter any Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (int j = 1; j <= number; j++)
+            {
+                fact = fact * j;
+            }
+            Console.WriteLine("Factorial of "+ number +" is: "+ fact);
+        }
+    }
+}
